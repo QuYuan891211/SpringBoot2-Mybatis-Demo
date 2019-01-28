576ad3427ca3f2ee3705bfde5551c47417161020
quyuan <quyuan891211@163.com>
2019-01-28 23:24:53 +0800

    版本更正

--- src/main/java/com/qy/spring2mybatisdemo/Spring2MybatisDemoApplication.java
+++ src/main/java/com/qy/spring2mybatisdemo/Spring2MybatisDemoApplication.java
@@ -3,6 +3,7 @@ package com.qy.spring2mybatisdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.qy.dao")
